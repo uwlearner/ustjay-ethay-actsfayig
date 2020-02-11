29d4096d0df5bbf2a260d95cc3396e0a4fbdde6f
uwlearner <>
2020-02-10 20:37:20 -0800

    initial commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 import requests
 from flask import Flask, send_file, Response
@@ -14,15 +15,35 @@ def get_fact():
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
 
-    return facts[0].getText()
+    return facts[0].getText().strip()
+
+
+def pig_latinize(fact):
+    with requests.Session() as session:
+        response = session.post("https://hidden-journey-62459.herokuapp.com/piglatinize/",
+                                data={"input_text": fact})
+
+    return response
 
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact()
+    pig_latinized = pig_latinize(fact)
+
+    body = re.sub('<head>', '<head>\n<style type ="text/css" >\n  .footer{\n      position:fixed;\n      bottom:10px;\n  }\n</style>', pig_latinized.text)
+    body = re.sub('</body>', '<p>URL: <a href={url}>{url}</a>\n\n<p><div class="footer">riginalOay actfay: {fact}</div></body>'.format(url=pig_latinized.url, fact=fact),
+                  body)
+
+    return Response(response=body.encode('utf8'), mimetype="text/html")
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
+
+
+
+
+
 
